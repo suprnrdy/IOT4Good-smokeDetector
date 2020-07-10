86698d6a5b99f5680cdc6e7ab3814c4e39772ece
Bryan Bui <>
2020-07-10 14:47:19 -0700

    - Reorganized code structure - GPS is sent only on first init, or if device moves > 10m and sends every specified period of time. - We send all 6 calculated PPM CO, Alcohol, CO2, Tolueno, NH4, Acetona every 10 minutes. - We send a heart beat every 10 minutes

--- smokeDetector.ino
+++ smokeDetector.ino
@@ -16,26 +16,32 @@ CayenneLPP lpp(51);
 TinyGPSPlus gps;
 DynamicJsonDocument jsonBuffer(4096);
 static const uint32_t GPSBaud = 4800;
-// A sample NMEA stream.
-const char *gpsStream =
-  "$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n"
-  "$GPGGA,045104.000,3014.1985,N,09749.2873,W,1,09,1.2,211.6,M,-22.5,M,,0000*62\r\n"
-  "$GPRMC,045200.000,A,3014.3820,N,09748.9514,W,36.88,65.02,030913,,,A*77\r\n"
-  "$GPGGA,045201.000,3014.3864,N,09748.9411,W,1,10,1.2,200.8,M,-22.5,M,,0000*6C\r\n"
-  "$GPRMC,045251.000,A,3014.4275,N,09749.0626,W,0.51,217.94,030913,,,A*7D\r\n"
-  "$GPGGA,045252.000,3014.4273,N,09749.0628,W,1,09,1.3,206.9,M,-22.5,M,,0000*6F\r\n";
+double dev_lat = 0.0, dev_lon = 0.0;
+bool first_lock = true;
 
 // MQ-135 Defines
 //Definitions
 #define placa "Arduino UNO"
-#define Voltage_Resolution 3.5
-#define analog_pin A2 //Analog input 0 of your arduino
+#define Voltage_Resolution 3.6
+#define analog_pin A2 // B-L072Z-LRWAN1 has only 3 Analog pins exposed, so use A2 in arduino library == PA_4 on board
 #define type "MQ-135" //MQ135
 #define ADC_Bit_Resolution 12 // For arduino UNO/MEGA/NANO
 #define RatioMQ135CleanAir 3.6//RS / R0 = 3.6 ppm  
 //Declare Sensor
 MQUnifiedsensor MQ135(placa, Voltage_Resolution, ADC_Bit_Resolution, analog_pin, type);
 
+float CO = 0.0;
+float Alcohol = 0.0;
+float CO2 = 0.0;
+float Tolueno = 0.0;
+float NH4 = 0.0;
+float Acetona = 0.0;
+
+// Timers
+static const unsigned long REFRESH_INTERVAL = 5000; // ms
+static unsigned long lastRefreshTime = 0;
+static unsigned long lostGPSTime = 0;
+static unsigned long dataTimer = 0;
 
 void setup( void )
 {
@@ -47,11 +53,9 @@ void setup( void )
     // Setup MQ-135
     //Set math model to calculate the PPM concentration and the value of constants
     MQ135.setRegressionMethod(1); //_PPM =  a*ratio^b
-    MQ135.setA(605.18); MQ135.setB(-3.937); // Configurate the ecuation values to get CO concentration
     MQ135.init(); 
     MQ135.setRL(1);
     MQ135.setR0(45.5);
-    
   
     // US Region
     LoRaWAN.begin(US915);
@@ -67,71 +71,125 @@ void setup( void )
     Serial.println("JOIN( )");
 }
 
-float CO = 0.0;
+void getAirData() {
+  // Collect data from Air Quality Sensor
+  MQ135.update(); // Update data, the arduino will be read the voltage on the analog pin
+  MQ135.setA(605.18); MQ135.setB(-3.937); // Configurate the ecuation values to get CO concentration
+  CO = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+  MQ135.setA(77.255); MQ135.setB(-3.18); // Configurate the ecuation values to get Alcohol concentration
+  Alcohol = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+  MQ135.setA(110.47); MQ135.setB(-2.862); // Configurate the ecuation values to get CO2 concentration
+  CO2 = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+  MQ135.setA(44.947); MQ135.setB(-3.445); // Configurate the ecuation values to get Tolueno concentration
+  Tolueno = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+  MQ135.setA(102.2 ); MQ135.setB(-2.473); // Configurate the ecuation values to get NH4 concentration
+  NH4 = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+  MQ135.setA(34.668); MQ135.setB(-3.369); // Configurate the ecuation values to get Acetona concentration
+  Acetona = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+}
 
 void sendPacket() {
   if (LoRaWAN.joined() && !LoRaWAN.busy())
   {
-    Serial.println("setting up packet");
-    lpp.reset();
-    CO = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
-    lpp.addAnalogInput(3, LoRaWAN.lastRSSI());
-    lpp.addAnalogInput(2, CO);
-    // Setup LPP Message
-    if (gps.location.isValid())
-    { 
-      Serial.print(gps.location.lat(), 6);
-      Serial.print(F(","));
-      Serial.println(gps.location.lng(), 6);
-      lpp.addGPS(1, gps.location.lat(), gps.location.lng(), gps.altitude.meters());
-      JsonArray root = jsonBuffer.to<JsonArray>();
-      lpp.decode(lpp.getBuffer(), lpp.getSize(), root);
-      serializeJsonPretty(root, Serial);
-      Serial.println();
-    } 
-    
-    Serial.print("TRANSMIT( ");
-    Serial.print("TimeOnAir: ");
-    Serial.print(LoRaWAN.getTimeOnAir());
-    Serial.print(", NextTxTime: ");
-    Serial.print(LoRaWAN.getNextTxTime());
-    Serial.print(", MaxPayloadSize: ");
-    Serial.print(LoRaWAN.getMaxPayloadSize());
-    Serial.print(", DR: ");
-    Serial.print(LoRaWAN.getDataRate());
-    Serial.print(", TxPower: ");
-    Serial.print(LoRaWAN.getTxPower(), 1);
-    Serial.print("dbm, UpLinkCounter: ");
-    Serial.print(LoRaWAN.getUpLinkCounter());
-    Serial.print(", DownLinkCounter: ");
-    Serial.print(LoRaWAN.getDownLinkCounter());
-    Serial.print(", RSSI: ");
-    Serial.print(LoRaWAN.lastRSSI());
-    Serial.print(", CO: ");
-    Serial.print(CO);
-    
-    Serial.println(" )");
-
-    // Send Packet
-    LoRaWAN.sendPacket(1, lpp.getBuffer(), lpp.getSize(), true);
+    if(lpp.getSize() > 0) {
+      Serial.println("sendPacket");
+      Serial.println("setting up packet");
+       
+      Serial.print("TRANSMIT( ");
+      Serial.print("TimeOnAir: ");
+      Serial.print(LoRaWAN.getTimeOnAir());
+      Serial.print(", MaxPayloadSize: ");
+      Serial.print(LoRaWAN.getMaxPayloadSize());
+      Serial.print(", DR: ");
+      Serial.print(LoRaWAN.getDataRate());
+      Serial.print("dbm, UpLinkCounter: ");
+      Serial.print(LoRaWAN.getUpLinkCounter());
+      Serial.print(", RSSI: ");
+      Serial.print(LoRaWAN.lastRSSI());
+      Serial.println(" )");
+//      Serial.println(lpp.getSize());
+      // Send Packet
+      LoRaWAN.sendPacket(1, lpp.getBuffer(), lpp.getSize(), true);  
+    }
   }
 }
 
-static const unsigned long REFRESH_INTERVAL = 5000; // ms
-static unsigned long lastRefreshTime = 0;
+void airqualityMessage() {
+  getAirData();
+  // Send air quality data every x minutes
+  // Repackage air quality data
+  lpp.addAnalogInput(3, CO);
+  lpp.addAnalogInput(4, Alcohol);
+  lpp.addAnalogInput(5, CO2);
+  lpp.addAnalogInput(6, Tolueno);
+  lpp.addAnalogInput(7, NH4);
+  lpp.addAnalogInput(8, Acetona);
+  
+  Serial.print("CO: ");
+  Serial.print(CO);
+  Serial.print(", Alcohol: ");
+  Serial.print(Alcohol);
+  Serial.print(", CO2: ");
+  Serial.print(CO2);
+  Serial.print(", Tolueno: ");
+  Serial.print(Tolueno);
+  Serial.print(", NH4: ");
+  Serial.print(NH4);
+  Serial.print(", Acetona: ");
+  Serial.println(Acetona);
+}
 
+void gpsMessage() {
+  if (gps.location.isValid())
+  { 
+    dev_lat = gps.location.lat();
+    dev_lon = gps.location.lng();
+    lpp.addGPS(1, gps.location.lat(), gps.location.lng(), gps.altitude.meters());
+  }
+}
+  
 void loop( void )
 {
   while (Serial1.available() > 0)
   {
+    // Gather our GPS data and encode it
     gps.encode(Serial1.read());
   }
 
+  lpp.reset();
+  // This is our heartbeat that sends us the RSSI from acked messages (probably don't need this, but nice to have right now while DC are free)
+  if(millis() - lastRefreshTime > 600000) { //1m = 60000, 1hr = 36000000, 10m = 600000
+    lastRefreshTime = millis();
+    lpp.addAnalogInput(2, LoRaWAN.lastRSSI());
+    // Build packet here, setup LPP Message
+  }
   
-  if(millis() - lastRefreshTime == REFRESH_INTERVAL) {
-    lastRefreshTime += REFRESH_INTERVAL;
-    MQ135.update(); // Update data, the arduino will be read the voltage on the analog pin
-    sendPacket();
-  } 
+  //get and send air quality data every x seconds
+  if(millis() - dataTimer > 600000) { //1m = 60000, 1hr = 36000000, 10m = 600000
+    dataTimer = millis();
+    airqualityMessage();
+  }
+  //get and send gps data once, then every x minutes if it changes. if it stops moving, don't send any updated packets. 
+  // If this is the first time to get a GPS Fix, lets set this location
+  if(first_lock) {
+    gpsMessage();
+    first_lock = false;
+  } else {
+    // We check if we've moved more than 10 m
+    double changed_distance = TinyGPSPlus::distanceBetween(gps.location.lat(), gps.location.lng(), dev_lat, dev_lon);
+    if(changed_distance > 10) {
+      // Update coordinates
+      gpsMessage();
+      // Report every minute/hour/etc....
+      if(millis() - lostGPSTime > 1200000) { //1m = 60000, 1hr = 36000000, 20m = 1200000
+        lostGPSTime = millis();
+        lpp.addGPS(1, gps.location.lat(), gps.location.lng(), gps.altitude.meters());
+      }
+    }
+  }
+  
+  // complete our packets here, and send. 
+  sendPacket();
+  
 }
  
